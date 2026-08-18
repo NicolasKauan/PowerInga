94b8686956c0e79ba6e7a9754a93d2b942c0a00f
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-08-17 21:01:10 -0300

    Primeiro commit criando o projeto em Spring boot contendo as dependências: Lombok, SpringWeb, SpringSecurity, MongoDB, Spring Data JPA, Spring Data MongoDB.

---
+++ nika/src/test/java/com/poweringa/nika/NikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poweringa.nika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
