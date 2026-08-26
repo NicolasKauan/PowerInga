22a6732ab1310fa244368daa730746709cf7c18a
Gustavo <gustavogomes1108@gmail.com>
2026-08-26 15:39:25 -0300

    #Feat: adição de um handler global para exceções

---
+++ poweringa-api/src/main/java/com/poweringa/api/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.poweringa.api.handlers;
+
+import com.poweringa.api.exceptions.UserAlreadyExists;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserAlreadyExists.class)
+    public ResponseEntity<String> handleUserAlreadyExists(Exception exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+}
