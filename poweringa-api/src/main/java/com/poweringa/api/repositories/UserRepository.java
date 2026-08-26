c8bf88dbfbd654d6c21f40b041f0a33492cea900
Gustavo <gustavogomes1108@gmail.com>
2026-08-26 15:39:12 -0300

    #Feat: adição do repository para usuário e alguns query methods

---
+++ poweringa-api/src/main/java/com/poweringa/api/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.poweringa.api.repositories;
+
+import com.poweringa.api.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
