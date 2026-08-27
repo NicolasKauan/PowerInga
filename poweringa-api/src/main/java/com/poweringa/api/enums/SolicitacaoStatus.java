40c7fa8fdeb27b858cf3c1f18c5895603fa7df58
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-27 14:38:59 -0300

    #Feat: criação da classe de solicitação

---
+++ poweringa-api/src/main/java/com/poweringa/api/enums/SolicitacaoStatus.java
@@ -0,0 +1,7 @@
+package com.poweringa.api.enums;
+
+public enum SolicitacaoStatus {
+    APROVADO,
+    REPROVADO,
+    PENDENTE
+}
---
+++ poweringa-api/src/main/java/com/poweringa/api/models/Solicitacao.java
@@ -0,0 +1,9 @@
+package com.poweringa.api.models;
+
+import com.poweringa.api.enums.SolicitacaoStatus;
+
+public class Solicitacao {
+    private String descricao;
+    private SolicitacaoStatus status;
+    //TODO - Campo de idCategoria
+}
