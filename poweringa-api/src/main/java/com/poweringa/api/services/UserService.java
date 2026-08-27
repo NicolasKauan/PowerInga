c9bb18bac9ff471939e1597cc093baaaecc79e4e
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-27 14:38:16 -0300

    #Feat: um endpoint para listar todos os usuários

--- poweringa-api/src/main/java/com/poweringa/api/services/UserService.java
+++ poweringa-api/src/main/java/com/poweringa/api/services/UserService.java
@@ -1,5 +1,5 @@
 package com.poweringa.api.services;
-
+import com.poweringa.api.models.User;
 import com.poweringa.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,10 +7,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
