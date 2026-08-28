ba4f1371447a00981678e161228f42ad917185eb
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-28 13:29:26 -0300

    #Feat: lógica do token JWT no login

--- poweringa-api/src/main/java/com/poweringa/api/security/SecurityFilter.java
+++ poweringa-api/src/main/java/com/poweringa/api/security/SecurityFilter.java
@@ -1,9 +1,15 @@
 package com.poweringa.api.security;
 
+import com.poweringa.api.services.TokenService;
+import com.poweringa.api.services.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,16 +17,35 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
 
+        if (token != null) {
+            String email = tokenService.validateToken(token);
+
+            UserDetails user = userService.loadUserByUsername(email);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
     }
 
     public String recoverToken(HttpServletRequest request) {
         String authHeader = request.getHeader("Authorization");
 
-        if (request == null) {
+        if (authHeader == null) {
             return null;
         }
 
