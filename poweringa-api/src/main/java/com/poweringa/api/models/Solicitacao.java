3857d3737d0f0a34459be7eed72c080600b85a63
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-31 10:04:02 -0300

    #Feat: model de solicitação

--- poweringa-api/src/main/java/com/poweringa/api/models/Solicitacao.java
+++ poweringa-api/src/main/java/com/poweringa/api/models/Solicitacao.java
@@ -1,9 +1,20 @@
 package com.poweringa.api.models;
 
 import com.poweringa.api.enums.SolicitacaoStatus;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document(collection = "solicitacoes")
 public class Solicitacao {
+    @Id
+    private String id;
     private String descricao;
-    private SolicitacaoStatus status;
+    private SolicitacaoStatus status = SolicitacaoStatus.PENDENTE;
     //TO DO - Campo de idCategoria
+
+    public Solicitacao(String descricao) {
+        this.descricao = descricao;
+    }
 }
