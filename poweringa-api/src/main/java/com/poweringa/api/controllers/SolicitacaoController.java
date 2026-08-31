85cef508d3f29ee84846f5bc312d534f94fbc049
Gustavo de Almeida Gomes <gustavogomes1108@gmail.com>
2026-08-31 10:30:23 -0300

    #Feat: rota de GET no controller para as solicitacoes

--- poweringa-api/src/main/java/com/poweringa/api/controllers/SolicitacaoController.java
+++ poweringa-api/src/main/java/com/poweringa/api/controllers/SolicitacaoController.java
@@ -6,16 +6,20 @@ import com.poweringa.api.services.SolicitacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/solicitacoes")
 public class SolicitacaoController {
     @Autowired
     private SolicitacaoService solicitacaoService;
+
+    @GetMapping
+    public ResponseEntity<List<Solicitacao>> findAll(@AuthenticationPrincipal User usuarioLogado) {
+        return ResponseEntity.ok(solicitacaoService.findAll(usuarioLogado));
+    }
 
     @PostMapping
     public ResponseEntity<Solicitacao> create(
