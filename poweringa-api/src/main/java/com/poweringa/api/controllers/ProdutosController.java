f625f25b211697a51b54367397e8d9164a86000c
Nicolas Kauan <nicolaskauanpro@gmail.com>
2026-08-31 17:57:27 -0300

    Feito a busca por id, a criação e o delete para os produtos, nesse momento temos somente os status basicos para os endpoint, podendo fazer o refino para os erros posteriormente

--- poweringa-api/src/main/java/com/poweringa/api/controllers/ProdutosController.java
+++ poweringa-api/src/main/java/com/poweringa/api/controllers/ProdutosController.java
@@ -4,11 +4,12 @@ import com.poweringa.api.models.Produtos;
 import com.poweringa.api.repositories.ProdutosRepository;
 import com.poweringa.api.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.plaf.PanelUI;
 import java.util.List;
 
 @RestController
@@ -17,10 +18,31 @@ public class ProdutosController {
     @Autowired
     private ProdutoService produtoService;
 
+    // POST /produtos
+    @PostMapping
+    public ResponseEntity<Produtos> save(@RequestBody Produtos produto){
+        Produtos produtoSalvo = produtoService.save(produto);
+        //Retorna o status 201(Criado) e o produto salvo no corpo da resposta .body
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvo);
+    }
+
+    //Get /produtos
     @GetMapping
     public ResponseEntity<List<Produtos>> findAll(){
+        //Api restfull devolvendo 200 de conseguiu encontrar
         return ResponseEntity.ok(produtoService.findAll());
     }
 
+    //GET /produtos/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Produtos> findById(@PathVariable String id){
+        return ResponseEntity.ok(produtoService.findById(id));
+    }
 
+    //DELETE /produtos/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Produtos> delete(@PathVariable String id){
+        //Status 204 
+        return ResponseEntity.noContent().build();
+    }
 }
--- poweringa-api/src/main/java/com/poweringa/api/services/ProdutoService.java
+++ poweringa-api/src/main/java/com/poweringa/api/services/ProdutoService.java
@@ -13,8 +13,28 @@ public class ProdutoService {
     @Autowired
     private ProdutosRepository produtosRepository;
 
+    //Get /produtos no service
     public List<Produtos> findAll(){
         return produtosRepository.findAll();
+    }
+
+    //GET /produtos/{id}
+    public Produtos findById(String id){
+        //Retorna o produto pelo id ou um erro
+        return produtosRepository.findById(id)
+                //Como existe uma chance do ID não existir, temos esse
+                // filtro para o Java lançar um erro automático para o produto não encontrado
+                .orElseThrow( () -> new RuntimeException("Produto não encontrado!"));
+    }
+
+    //POST /produtos
+    public Produtos save(Produtos produto){
+        return produtosRepository.save(produto);
+    }
+
+    //DELETE /produtos/id
+    public void delete(String id){
+        produtosRepository.deleteById(id);
     }
 
 }
